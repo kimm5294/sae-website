26ef25696ae4abcd607b7145a7ee91ac5a329808
Matt Kim <kimm5294@gmail.com>
2017-07-28 11:01:43 -0400

    add linkedin to migration

--- db/migrate/20170611162411_create_users.rb
+++ db/migrate/20170611162411_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :last_name, null: false
       t.string :email, null: false
       t.string :password_digest, null: false
+      t.string :linkedin, null: false
 
       t.timestamps(null: false)
     end
