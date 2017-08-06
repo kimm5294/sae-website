1a68391883cff39b3f93c8585c69c2679b56a36b
Matt Kim <kimm5294@gmail.com>
2017-08-06 08:01:09 -0400

    create routes in users controller for new, create, show and also make user params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:current_user_id] = @user.id
+      redirect_to @user
+    else
+      @errors = @user.errors.full_messages
+      render "new"
+    end
+
+    def show
+      if logged_in?
+        @user = User.find_by(id: current_user_id)
+        render "show"
+      else
+        redirect_to "/"
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :first_name, :last_name, :grad_year, :password, :password_confirmation)
+    end
+
 end
