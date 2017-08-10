56f0a16b7a240ebdd9df77c27dca204319f09a7f
Matt Kim <kimm5294@gmail.com>
2017-08-10 16:30:20 -0400

    create new, create and destroy routes in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user
+      if @user.authenticate(params[:password])
+        session[:current_user_id] = @user.id
+        redirect_to @user
+      else
+        @errors = ["Email and password combination do not match our records"]
+        render "new"
+      end
+    else
+      @errors = ["Email does not match our records"]
+      render "new"
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to '/'
+  end
+
 end
